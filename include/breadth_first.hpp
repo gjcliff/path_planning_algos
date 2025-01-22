7f6e50c51b96d2b2c5c7cea398d65a7ca96b8684
Graham Clifford <gjcliff@gmail.com>
2025-01-22 10:41:01 -0500

    breadth first search is working

--- include/breadth_first.hpp
+++ include/breadth_first.hpp
@@ -8,20 +8,44 @@
 struct breadth_hash {
 public:
   template <typename T, typename U>
-  std::size_t operator()(const std::pair<T, U> &x) const
+  int operator()(const std::pair<T, U> &x) const
   {
     return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
   }
 };
 
 // struct practice {
-//   public:
-//     template <typename T, typename U>
-//       std::size_t operator()(const std::pair<T,U> &x)
-// }
+// public:
+//   template <typename T, typename U>
+//   int operator()(const std::pair<T, U> &x) const
+//   {
+//     return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+//   }
+// };
+//
+// struct more_practice {
+// public:
+//   template <typename T, typename U>
+//   std::size_t operator()(const std::pair<T, U> &x) const
+//   {
+//     return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+//   }
+// };
+//
+// struct more_more {
+// public:
+//   template <typename T, typename U>
+//     int operator()(const std::pair<T, U> &x) const
+//     {
+//       return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+//     }
+// };
 
 struct Node {
-  Node(int dist, std::shared_ptr<Node> prev) : dist(dist), prev(prev) {}
+  Node(int dist, std::pair<int, int> coord, std::shared_ptr<Node> prev)
+    : dist(dist), coord(coord), prev(prev)
+  {
+  }
   int dist;
   std::pair<int, int> coord;
   std::shared_ptr<Node> prev;
@@ -46,12 +70,13 @@ public:
     std::vector<std::pair<int, int>> directions{
       {0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-    std::shared_ptr<Node> current = std::make_shared<Node>(0, nullptr);
+    std::shared_ptr<Node> current =
+      std::make_shared<Node>(0, start_pos, nullptr);
 
     std::queue<std::shared_ptr<Node>> queue;
     queue.push(current);
 
-    std::unordered_set<std::pair<int,int>, pairhash> visited;
+    std::unordered_set<std::pair<int, int>, pairhash> visited;
 
     while (!queue.empty()) {
       int rows = grid_->rows;
@@ -59,6 +84,8 @@ public:
 
       current = queue.front();
       queue.pop();
+
+      visited.insert(current->coord);
 
       if (current->coord == goal_pos) {
         std::cout << "Shortest path distance: " << current->dist << std::endl;
@@ -68,19 +95,22 @@ public:
           current = current->prev;
           grid_->grid.at(start_pos.first).at(start_pos.second) = 3;
           grid_->grid.at(goal_pos.first).at(goal_pos.second) = 4;
-          return;
         }
+        return;
       }
 
       for (const auto &dir : directions) {
+        std::pair<int, int> next_coord = {current->coord.first + dir.first,
+                                          current->coord.second + dir.second};
         std::shared_ptr<Node> next =
-          std::make_shared<Node>(current->dist + 1, current);
+          std::make_shared<Node>(current->dist + 1, next_coord, current);
 
         if (next->coord.first >= 0 && next->coord.first < rows &&
             next->coord.second >= 0 && next->coord.second < cols &&
-            grid_->grid.at(next->coord.first).at(next->coord.second) != 1
-            && visited.find(next->coord) != visited.end()) {
-          queue.push(current);
+            visited.find(next->coord) == visited.end()) {
+          if (grid_->grid.at(next->coord.first).at(next->coord.second) != 1) {
+            queue.push(next);
+          }
         }
       }
     }
